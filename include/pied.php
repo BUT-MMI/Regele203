fecc038f56f25442cd5def819c1229113f13d490
Ulyxx3 <ulyssesoucaret@gmail.com>
2026-04-02 10:21:10 +0200

    footer updt

--- include/pied.php
+++ include/pied.php
@@ -2,8 +2,9 @@
 <footer>
   <p>
     Crousti Movies &mdash; SAÉ 203 &mdash; BUT MMI &mdash;
-    Prénom Nom &lt;<a href="mailto:prenom.nom@etu.univ-amu.fr">prenom.nom@etu.univ-amu.fr</a>&gt;
+    Ulysse Soucaret &lt;<a href="mailto:ulysse.soucaret@etu.univ-amu.fr">ulysse.soucaret@etu.univ-amu.fr</a>&gt;
   </p>
 </footer>
 </body>
+
 </html>
